91d3a6b9e78118f4e1e60eb0444623f4ef18f62f
Under_Koen <koenvanstaveren@hotmail.com>
2018-07-19 16:56:58 +0200

    Basic setup
    Signed-off-by: Under_Koen <koenvanstaveren@hotmail.com>

---
+++ Core/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Under_Koen on 19/07/2018.
+ */
+module edulogo.core {
+    requires slf4j.api;
+}
---
+++ Core/src/main/java/nl/edulogo/core/Main.java
@@ -0,0 +1,12 @@
+package nl.edulogo.core;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Under_Koen on 19/07/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        LoggerFactory.getLogger(Main.class).info("test");
+    }
+}
